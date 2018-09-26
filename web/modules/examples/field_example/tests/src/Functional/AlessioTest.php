9d73847f0a5df9c53cfd249c011814bcc00f7274
a.frabotta <a.frabotta@eustema.it>
2018-09-26 10:24:21 +0200

    add test di prova

---
+++ web/modules/examples/field_example/tests/src/Functional/AlessioTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\Tests\field_example\Functional;
+
+/**
+ * Test base scritto da Alessio 
+ *
+ *
+ * @group field_example
+ * @group examples
+ *
+ * @ingroup field_example
+ */
+class AlessioTest extends TestCase {
+
+  /**
+   * Test di base molto semplice.
+   *
+   */
+  public function testNumeroUno() {
+    $assert = $this->assertSession();
+
+    $this->assertEquals(1, 1);
+    
+  }
+
+}
